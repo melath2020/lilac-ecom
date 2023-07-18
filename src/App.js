911816f5aa32a216b0e4092deaf2e4150d54eed1
Aswin s <aswinsudhakaran98@gmail.com>
2023-07-18 11:32:39 +0530

    basic-update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
   <>
   <BrowserRouter>
   <Routes>
-    
+    <Route path='/' element={<Layout/>}>
+      <Route index element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+    </Route>
   </Routes>
   </BrowserRouter>
   </>
