5d49dc2a2147e33db34d28e3c58397d2626051b2
Aswin s <aswinsudhakaran98@gmail.com>
2023-07-19 05:24:31 +0530

    update-first-head

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
-
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { IoIosCall, IoMdArrowDropdown } from "react-icons/io";
+import { MdEmail, MdLocationOn } from "react-icons/md";
+import { FaLocationDot } from "react-icons/fa";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <header className="header-top-strip pt-2">
+        <div className="container">
+          <div className="row text-white main-row">
+            <div className="col-6">
+              <div className="row">
+                <div className="col-3">
+                  <p>
+                    <IoIosCall />
+                    +221 33 66 22
+                  </p>
+                </div>
+                <div className="col-4">
+                  <p>
+                    <MdEmail /> support@elextra.io
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-6 text-end">
+              <div className="row text-end">
+                <div className="col-7 ">
+                  <p>
+                    <MdLocationOn />
+                    &nbsp;  Locations
+                  </p>
+                </div>
+                <div className="col-3">
+                  <p className="text-end">$ &nbsp; Dollar (US)</p>
+                </div>
+                <div className="col-2">
+                  <p>
+                    En&nbsp; 
+                    <IoMdArrowDropdown />
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
 
-export default Header
+      <header className="header-upper">
+        <div className="container">
+          <div className="row">
+            
+          </div>
+        </div>
+      </header>
+
+
+    </>
+  );
+};
+
+export default Header;
