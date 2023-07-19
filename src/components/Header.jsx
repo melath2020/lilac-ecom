88dd1d5929433cb8627e22a2461202c3ee3a493d
Aswin s <aswinsudhakaran98@gmail.com>
2023-07-19 09:18:25 +0530

    header-bottom-update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { IoIosCall, IoMdArrowDropdown } from "react-icons/io";
 import { MdEmail, MdLocationOn } from "react-icons/md";
-import { FaLocationDot } from "react-icons/fa";
+import { BiSearchAlt2 } from "react-icons/bi";
+import { AiOutlineHeart } from "react-icons/ai";
+import { GiBasket } from "react-icons/gi";
+import { CgProfile } from "react-icons/cg";
+import logo from "../images/logo-6 1.png";
 const Header = () => {
   return (
     <>
@@ -29,7 +33,7 @@ const Header = () => {
                 <div className="col-7 ">
                   <p>
                     <MdLocationOn />
-                    &nbsp;  Locations
+                    &nbsp; Locations
                   </p>
                 </div>
                 <div className="col-3">
@@ -37,7 +41,7 @@ const Header = () => {
                 </div>
                 <div className="col-2">
                   <p>
-                    En&nbsp; 
+                    En&nbsp;
                     <IoMdArrowDropdown />
                   </p>
                 </div>
@@ -50,12 +54,84 @@ const Header = () => {
       <header className="header-upper">
         <div className="container">
           <div className="row">
-            
+            <div className="col-2">
+              <img src={logo} alt="" srcset="" className="logo-main" />
+            </div>
+            <div className="col-2">
+              <div className="dropdown mt-4">
+                <button
+                  className="btn btn-clsfds-drpdwn dropdown-toggle "
+                  type="button"
+                  id="dropdownMenuButton"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Classifieds &nbsp;
+                </button>
+                <div
+                  className="dropdown-menu"
+                  aria-labelledby="dropdownMenuButton"
+                >
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-4">
+              <div class="input-group mb-3 mt-4">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Search Here..."
+                  aria-label="Search"
+                  aria-describedby="basic-addon2"
+                />
+                <span class="input-group-text" id="basic-addon2">
+                  <BiSearchAlt2 className="search-icon" />
+                </span>
+              </div>
+            </div>
+            <div className="col-2 mt-4 text-end ">
+              <AiOutlineHeart className="ms-3 text-dark-emphasis" />
+              <GiBasket className="ms-3 text-dark-emphasis" />
+              <CgProfile className="ms-3 text-dark-emphasis" />
+            </div>
+            <div className="col-2 mt-2 p-2 text-end">
+              <button type="button" class=" btn-classifieds text-white ">
+                Classifieds
+              </button>
+            </div>
           </div>
         </div>
       </header>
 
-
+      <header className="header-bottom ">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div class="d-flex flex-row mb-3">
+                <div class="p-2">All Categories </div>
+                <div class="p-2">Flex item 2</div>
+                <div class="p-2">Flex item 3</div>
+                <div class="p-2">Flex item 3</div>
+                <div class="p-2">Flex item 3</div>
+                <div class="p-2">Flex item 3</div>
+                <div class="p-2">Flex item 3</div>
+                <div class="p-2">Flex item 3</div>
+                <div class="p-2">Flex item 3</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
     </>
   );
 };
